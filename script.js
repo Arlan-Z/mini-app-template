0c30e3f064351436beebec4091cc4dda1e4058a3
Arlan <145493122+Arlan-Z@users.noreply.github.com>
2024-10-19 03:59:51 +0500

    logic changed

--- script.js
+++ script.js
@@ -1,35 +1,40 @@
 const data = document.getElementById("data");
 window.onload = function() {
-    // Инициализируем Telegram Web App
-    Telegram.WebApp.ready();
+    if (typeof Telegram !== "undefined" && Telegram.WebApp) {
+        // Инициализируем Telegram Web App
+        Telegram.WebApp.ready();
 
-    // Получаем initData
-    const initData = Telegram.WebApp.initData;
-    data.innerHTML = JSON.stringify(initData);
-    // Проверяем наличие initData
-    if (initData) {
-        // Отправляем initData на сервер для валидации
-        fetch('https://demo-pp.onrender.com/auth/telegram', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ initData })
-        })
-        .then(response => response.json())
-        .then(data => {
-            if (data.success) {
-                alert('success');
-            } else {
-                // Обрабатываем ошибку
-                alert('Ошибка авторизации: ' + data.message);
-            }
-        })
-        .catch(error => {
-            console.error('Ошибка:', error);
-            alert('Произошла ошибка при авторизации.');
-        });
+        // Получаем initData
+        const initData = Telegram.WebApp.initData;
+        data.innerHTML = JSON.stringify(initData);
+
+        // Проверяем наличие initData
+        if (initData) {
+            // Отправляем initData на сервер для валидации
+            fetch('https://demo-pp.onrender.com/auth/telegram', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ initData })
+            })
+            .then(response => response.json())
+            .then(data => {
+                if (data.success) {
+                    alert('success');
+                } else {
+                    // Обрабатываем ошибку
+                    alert('Ошибка авторизации: ' + data.message);
+                }
+            })
+            .catch(error => {
+                console.error('Ошибка:', error);
+                alert('Произошла ошибка при авторизации.');
+            });
+        } else {
+            alert('initData отсутствует. Пожалуйста, попробуйте снова.');
+        }
     } else {
-        alert('initData отсутствует. Пожалуйста, попробуйте снова.');
+        alert('Скрипт должен выполняться в Telegram Web App.');
     }
 };
