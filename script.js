19d5c860b7cac86475ad6d34f98766c5c1c3fae5
Arlan <145493122+Arlan-Z@users.noreply.github.com>
2024-10-19 19:13:38 +0500

    test

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+try{
 const usernameElem = document.getElementById("username");
 initData = {"initData":"user=%7B%22id%22%3A1309966182%2C%22first_name%22%3A%22Arlan%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22Nearlan%22%2C%22language_code%22%3A%22ru%22%2C%22allows_write_to_pm%22%3Atrue%7D&chat_instance=3339528180163683006&chat_type=sender&auth_date=1729262581&hash=83bce350a0ef670d3d7e4ff73fd9bb242471958b8579697b755aeb2865b91598"}
 sendData(initData);
@@ -48,7 +49,10 @@ function sendData(initData) {
         // Обработка ответа от сервера
     })
     .catch(error => {
-        console.error('Error:', error);
+        alert.error('Error:', error);
         alert('Error: ' + error.message);
     });
+}
+} catch(error){
+    alert("Everything is broken: ", error);
 }
