5fa3ca35db1208eaa44687f7471efce8f9841f2a
Yerasyil Omirzhanov <65915633+Pingwinnel@users.noreply.github.com>
2024-10-19 20:10:29 +0500

    add footer to other tabs

--- Courses/courseItem.js
+++ Courses/courseItem.js
@@ -10,15 +10,15 @@ export async function getCourseDetails(courseId) {
     try {
         const response = await fetch(`${API_BASE_URL}/${courseId}`);
 
-        // Check if the response is OK (status code 200-299)
+
         if (!response.ok) {
             throw new Error(`Error: ${response.status} ${response.statusText}`);
         }
 
         const courseData = await response.json();
-        return courseData; // Return the course data
+        return courseData;
     } catch (error) {
         console.error('Failed to fetch course details:', error);
-        throw error; // Propagate the error
+        throw error;
     }
 }
