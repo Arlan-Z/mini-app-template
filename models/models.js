e41ce17dd08011e85c36d5453908cb569eefb413
Yerasyil Omirzhanov <65915633+Pingwinnel@users.noreply.github.com>
2024-10-19 21:25:33 +0500

    Update models.js

--- models/models.js
+++ models/models.js
@@ -1,27 +1,46 @@
-export const user={
-    "id": 1,
-    "firstName": "Иван",
-    "lastName": "Иванов",
-    "username": "ivanov",
-    "photoUrl": "http://example.com/photo.jpg",
-    "tokens": 100,
-    "level": 2,
-    "exp": 150,
-    "totalExp": 150,
-    "achievements": [],
-    "completedCourses": [],
-    "completedSections": [],
-    "enrolledCourses": []
+// Course Type
+class Course {
+    constructor(id, title, description, requiredLevel, expReward, rewardTokens, sections) {
+        this.id = id; // Long
+        this.title = title; // String
+        this.description = description; // String
+        this.requiredLevel = requiredLevel; // int
+        this.expReward = expReward; // int
+        this.rewardTokens = rewardTokens; // int
+        this.sections = sections; // Set of Section objects
+    }
 }
 
-
-
-export const course={
-    "title": "Курс по Spring Boot",
-    "description": "Подробный курс по разработке приложений на Spring Boot.",
-    "requiredLevel": 1,
-    "expReward": 200,
-    "rewardTokens": 100
+// Section Type
+class Section {
+    constructor(id, title, content, course, assignment) {
+        this.id = id; // Long
+        this.title = title; // String
+        this.content = content; // String (LOB)
+        this.course = course; // Course object (ManyToOne)
+        this.assignment = assignment; // Assignment object (OneToOne)
+    }
 }
 
-export default {user, course}
+// Assignment Type
+class Assignment {
+    constructor(id, question, answer, section, expReward, rewardTokens) {
+        this.id = id; // Long
+        this.question = question; // String
+        this.answer = answer; // String
+        this.section = section; // Section object (ManyToOne)
+        this.expReward = expReward; // int
+        this.rewardTokens = rewardTokens; // int
+    }
+}
+
+// User Type
+class User {
+    constructor(id, firstName, lastName, username, photoUrl) {
+        this.id = id; // Long
+        this.firstName = firstName; // String
+        this.lastName = lastName; // String
+        this.username = username; // String
+        this.photoUrl = photoUrl; // String
+    }
+}
